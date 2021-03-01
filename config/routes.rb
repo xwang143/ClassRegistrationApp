f3f950f5066329a66444db2d3f3670d04033c876
xwang143 <xwang143@brandeis.edu>
2021-02-28 23:19:01 -0500

    Add welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :subjects
   resources :courses
   resources :instructors
+  root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
