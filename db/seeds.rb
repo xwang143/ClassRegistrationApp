e10613457e51e91614382fe3c73dcc2525fb667b
xwang143 <xwang143@brandeis.edu>
2021-03-01 23:39:37 -0500

    Add delete_all to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+Instructor.delete_all
+Course.delete_all
+Subject.delete_all
+Category.delete_all
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
